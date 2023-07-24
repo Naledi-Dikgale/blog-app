e26e924399b54c02580b794594d8afd6e8992407
naledi-dikgale <naledi22khosi@gmail.com>
2023-07-24 15:17:17 +0200

    Fix linter errors

--- spec/requests/user_controller_spec.rb
+++ spec/requests/user_controller_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
- RSpec.describe UsersController, type: :controller do
-  describe "GET #index" do
-    it "assigns all users to @users" do
-      user1 = User.create(name: "John")
-      user2 = User.create(name: "Jane")
-       get :index
+RSpec.describe UsersController, type: :controller do
+  describe 'GET #index' do
+    it 'assigns all users to @users' do
+      user1 = User.create(name: 'John')
+      user2 = User.create(name: 'Jane')
+      get :index
       expect(assigns(:users)).to eq([user1, user2])
     end
-     it "renders the index template" do
+    it 'renders the index template' do
       get :index
       expect(response).to render_template(:index)
     end
   end
-   describe "GET #show" do
-    it "assigns the requested user to @user" do
-      user = User.create(name: "John")
-       get :show, params: { id: user.id }
+  describe 'GET #show' do
+    it 'assigns the requested user to @user' do
+      user = User.create(name: 'John')
+      get :show, params: { id: user.id }
       expect(assigns(:user)).to eq(user)
     end
-     it "renders the show template" do
-      user = User.create(name: "John")
-       get :show, params: { id: user.id }
+    it 'renders the show template' do
+      user = User.create(name: 'John')
+      get :show, params: { id: user.id }
       expect(response).to render_template(:show)
     end
   end
