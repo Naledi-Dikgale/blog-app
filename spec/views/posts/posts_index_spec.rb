1c053326b0298bcae0e146e707c240ddfb358c29
naledi-dikgale <naledi22khosi@gmail.com>
2023-07-24 15:12:14 +0200

    Fix n+1

--- spec/views/posts/posts_index_spec.rb
+++ spec/views/posts/posts_index_spec.rb
@@ -1,14 +1,44 @@
-RSpec.describe 'Posts', type: :feature do
-  before(:each) do
-    @user = User.create!(name: 'Naledi Dikgale')
-    @post1 = @user.posts.create!(title: 'Test Post 1', text: 'This is a test post 1.') 
-    @post2 = @user.posts.create!(title: 'Test Post 2', text: 'This is a test post 2.') 
-    # Create some comments for the posts
-    @comment1 = @post1.comments.create!(user: @user, content: 'Comment 1 for post 1')
-    @comment2 = @post1.comments.create!(user: @user, content: 'Comment 2 for post 1')
-    @comment3 = @post2.comments.create!(user: @user, content: 'Comment 1 for post 2')
+require 'rails_helper'
+
+RSpec.describe 'User', type: :feature, js: false do
+  before :each do
+    @user = User.create(name: 'Naledi Dikgale', photo: 'https://e0.pxfuel.com/wallpapers/820/800/desktop-wallpaper-cute-black-girls-black-cartoon-characters-thumbnail.jpg',
+                        bio: 'Undisputed Queen of DIY')
+    @first_post = Post.create(author: @user, title: 'Naledi', text: 'How to make a table')
+    @comment = Comment.create(post: @first_post, author: @user, text: 'Nice post!')
+
+    visit "/users/#{@user.id}/posts"
   end
 
-  it 'displays user information' do
-    visit user_posts_path(@user)
-    expect(page).to have_content(@user.name)
+  describe 'Post page' do
+    it "shows the user's profile" do
+      expect(page).to have_css("img[src*='https://e0.pxfuel.com/wallpapers/820/800/desktop-wallpaper-cute-black-girls-black-cartoon-characters-thumbnail.jpg']")
+    end
+
+    it "shows the user's username" do
+      expect(page).to have_content(@user.name)
+    end
+
+    it 'shows the number of post' do
+      expect(page).to have_content(@user.posts.count)
+    end
+
+    it 'shows the title of the post' do
+      expect(page).to have_content('Naledi')
+    end
+
+    it 'shows the body of the post' do
+      expect(page).to have_content('How to make a table')
+    end
+
+    it 'shows the first comment of the post' do
+      expect(page).to have_content(@comment.text)
+    end
+
+    it 'shows the number of likes & comments' do
+      user_post = @user.posts.find_by(id: @first_post.id)
+      expect(page).to have_content(user_post.comments.count)
+      expect(page).to have_content(user_post.likes.count)
+    end
+  end
+end
