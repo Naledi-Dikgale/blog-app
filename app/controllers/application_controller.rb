3c6a6e8e6f9154d28dd8678f5a088b0c5ede844c
naledi-dikgale <naledi22khosi@gmail.com>
2023-07-26 12:01:11 +0200

    Add devise to the project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-  def current_user
-    @current_user ||= User.first
-  end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
