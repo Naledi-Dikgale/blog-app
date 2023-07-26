ca6c859b16cf863a45cb35033157525a509c0237
naledi-dikgale <naledi22khosi@gmail.com>
2023-07-26 14:28:13 +0200

    Removed commented code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
-  # helper_method :current_user
-  # def current_user
-  #   @current_user ||= User.first
-  # end
-  before_action :authenticate_user! # , except: [:index, :show]
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
