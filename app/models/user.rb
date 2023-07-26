2aaecf01ee55041684af8261215c42e3c2d0d558
naledi-dikgale <naledi22khosi@gmail.com>
2023-07-26 16:16:38 +0200

    Implementd required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
