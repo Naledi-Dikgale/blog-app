cce5481b5650402b34767cdd1ba6a678dda17779
Lugard Agu <agulugard@gmail.com>
2023-07-26 11:10:00 +0100

    Add devise parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
- 
+  # helper_method :current_user
+  # def current_user
+  #   @current_user ||= User.first
+  # end
+  before_action :authenticate_user! # , except: [:index, :show]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
+  def configure_permitted_parameters
+    attributes = [:name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
