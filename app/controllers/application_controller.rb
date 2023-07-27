8e4541fb9da005aaa3434697255a7827c5a12eb6
naledi-dikgale <naledi22khosi@gmail.com>
2023-07-27 17:47:54 +0200

    Implement api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: %i[index show]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+  respond_to :json
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,14 @@ Rails.application.routes.draw do
   end
   # Defines the root path route ("/")
     root 'users#index'
+
+    namespace :api do
+     namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create] 
+        end
+      end
+     end
+    end
 end
